47c19521222a85b00e3799aa381a2243d0e293cf
Erik Michel <erikmichelrobado@gmail.com>
2024-10-22 11:55:20 +0200

    dynamodb export script

---
+++ dynamodb-table-export-csv/main.py
@@ -0,0 +1,36 @@
+import boto3
+import csv
+import argparse
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('coffee_pool-dev')
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--attributes',type=str)
+parser.add_argument('--limit', type=int)
+args = parser.parse_args()
+
+scan_params = {}
+scan_params['Limit'] = args.limit if args.limit else 100
+if args.attributes:
+    scan_params['Select'] = 'SPECIFIC_ATTRIBUTES'
+    scan_params['ProjectionExpression'] = args.attributes
+else:
+    scan_params['Select'] = 'ALL_ATTRIBUTES'
+
+response = table.scan(**scan_params)
+data = response['Items']
+
+while 'LastEvaluatedKey' in response:
+    scan_params['ExclusiveStartKey'] = response['LastEvaluatedKey']
+
+    response = table.scan(**scan_params)
+    data.extend(response['Items'])
+    print(f'Exporting data... {len(data)} items')
+
+csv_file = 'output.csv'
+with open(csv_file, 'w', newline='') as file:
+    fieldnames = data[0].keys()
+    writer = csv.DictWriter(file, fieldnames)
+    writer.writeheader()
+    writer.writerows(data)
