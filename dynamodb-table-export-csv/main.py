0537c86690e0b0976739f39c508107b586be0993
Erik Michel <erikmichelrobado@gmail.com>
2024-11-05 21:05:53 +0100

    added optional aws profile argument for session

--- dynamodb-table-export-csv/main.py
+++ dynamodb-table-export-csv/main.py
@@ -4,11 +4,15 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("table_name", help="Table name to export")
-parser.add_argument('--attributes',type=str)
-parser.add_argument('--limit', type=int)
+parser.add_argument('--attributes', type=str, help="A comma-separated list of attribute names to retrieve from the DynamoDB table. If specified, the scan will return only these attributes. If not provided, all attributes will be returned.")
+parser.add_argument('--limit', type=int, help="Maximum number of items to return from the scan operation. Default is 100.")
+parser.add_argument('--profile', type=str, help="AWS CLI profile to use for authentication. Defaults to 'default' if not specified.")
 args = parser.parse_args()
 
-dynamodb = boto3.resource('dynamodb')
+session = boto3.session.Session(
+    profile_name=args.profile if args.profile else 'default'
+)
+dynamodb = session.resource('dynamodb')
 table = dynamodb.Table(args.table_name)
 
 scan_params = {}
